f02bfc47953ace17b083bce9ab93b859db46b44f
Batsheva Rich <batsheva7424@gmail.com>
2020-06-21 02:23:51 +0300

    navbar + styling

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -1,29 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Home from './home/Home'
-import Title from './Title/Title';
+import { Route, NavLink, HashRouter } from "react-router-dom";
+import { Nav, Navbar } from 'react-bootstrap'
 function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <Title></Title>
+  //     </header>
+  //   </div>
+  // );
   return (
-    <div className="App">
-      <header className="App-header">
-        <Title></Title>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Home></Home>
-    </div>
-  );
+    <HashRouter>
+      <Nav bg="light" expand="lg" class="navbar-inverse">
+        <ul className="header">
+          <Navbar.Brand style={{color:'white'}} href="#home">Recipes</Navbar.Brand>
+          <li><NavLink to="/home">Home</NavLink></li>
+          <li><NavLink to="/stuff">Stuff</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+      </Nav>
+      <div className="content">
+        <Route path="/home" exact component={Home} />
+        {/* <Route path="/stuff" component={Stuff} />
+          <Route path="/contact" component={Contact} /> */}
+      </div>
+    </HashRouter>
+  )
 }
 
 export default App;
