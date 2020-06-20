c1d5820ae04e1ebcc03e9fae0876c75811aea69e
Batsheva Rich <batsheva7424@gmail.com>
2020-06-20 23:14:07 +0300

    added title

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Title from './Title/Title';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Title></Title>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
